885e3e6ca43e20962301436f496ef1db94c9125f
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-07-31 17:39:11 +0600

    get products

--- src/Components/AddProductLink/AddProductLink.jsx
+++ src/Components/AddProductLink/AddProductLink.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const AddProductLink = ({ itemID, setItemID }) => {
+const AddProductLink = ({ setItemID }) => {
     const link = 'https://detail.1688.com/offer/664956936566.html';
 
     // Extract the item ID from the link using the split() method
@@ -12,14 +12,15 @@ const AddProductLink = ({ itemID, setItemID }) => {
         const itemid = e.target.itemlink.value;
         // Extract the item ID from the link using the split() method
         const extId = link.split('/').pop().replace('.html', '');
-        console.log(itemid);
-        console.log(extId);
+        // console.log(itemid);
+        // console.log(extId);
         setItemID(extId);
 
     };
 
     return (
         <div className='w-full py-7'>
+            <h3 className="pb-5 text-3xl text-center text-gray-600">Enter Product Details URL</h3>
             <form className='w-full flex gap-3 justify-center items-center' onSubmit={getId} action="">
                 <input name='itemlink' type="text" placeholder="Type here" className="input input-bordered w-full max-w-md rounded" />
                 <button type='submit' className="btn btn-outline btn-info rounded">Get Info</button>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AddProductLink from '../../Components/AddProductLink/AddProductLink';
 
 const Home = () => {
-const [itemID, setItemID] = useState();
+    const [itemID, setItemID] = useState();
+    const [productData, setProductData] = useState(null);
 
-    // console.log("Item ID", itemId);
+
+    console.log("Item ID", itemID);
+
+    const apiKey = 'd5227a4d75d4e397254e059c2b1bf982';
+    const apiUrl = `https://www.lovbuy.com/1688api/getproductinfo.php?key=${apiKey}&item_id=${itemID}&lang=en`;
+
+    useEffect(() => {
+        // Fetch data using the GET method
+        fetch(apiUrl)
+            .then((response) => response.json())
+            .then((data) => setProductData(data))
+            .catch((error) => console.error('Error fetching data:', error));
+    }, [apiUrl]);
+
+    console.log(productData);
+
     return (
         <div className='w-full h-full'>
-            <AddProductLink itemID={itemID} setItemID={setItemID} />
+            <AddProductLink setItemID={setItemID} />
 
             <h3 className="text-rose-600 font-bold text-center text-7xl">This is Home Page</h3>
         </div>
